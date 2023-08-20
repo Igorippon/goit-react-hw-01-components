28c8609b2ab7d589b406e4dab7f433ce7c550f6e
Igorippon <botvunaigor@gmail.com>
2023-08-20 22:45:50 +0300

    Update TransactionHistory.js

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -12,21 +12,36 @@ export const TransactionHistory = ({ items }) => {
         </thead>
 
         <tbody>
-            {items.map(item => (
-                <Line key={item.id}>
-                    <Cell>{item.type}</Cell>
-                    <Cell>{item.amount}</Cell>
-                    <Cell>{item.currency.toUpperCase()}</Cell>
-                </Line>
-            ))
+            {items.map(item => {
+              return (<TableRow
+                 key = {item.id}
+                 type={item.type}
+                 currency={item.currency}
+                 amount={item.amount}
+                />); 
+            })
             }
         </tbody>
     </Table>
     );
 }
 
+const TableRow = ({type, amount,currency}) =>{
+    return (<Line >
+        <Cell>{type}</Cell>
+        <Cell>{amount}</Cell>
+        <Cell>{currency.toUpperCase()}</Cell>
+        </Line> );
+}
+
 TransactionHistory.propTypes = {
     items: PropTypes.array.isRequired,
 };
+
+TableRow.propTypes = {
+    type:PropTypes.string.isRequired,
+    amount:PropTypes.string.isRequired,
+    currency:PropTypes.string.isRequired,
+}
 
 
